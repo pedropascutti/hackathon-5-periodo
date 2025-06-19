65ec38699ae9011e7f096bb4105fb455e1b0e8b7
Gustavo Passos <143558941+GustavoPS18@users.noreply.github.com>
2025-06-19 17:14:22 -0300

    Deletando versão antiga para adicionar nova

--- flutter/lib/utilitarios/constantes.dart
+++
@@ -1,8 +0,0 @@
-class StringsApp {
-  static const String nomeApp = 'Corretor de Gabaritos';
-  static const String usuario = 'Usuário';
-  static const String senha = 'Senha';
-  static const String entrar = 'Entrar';
-  static const String sair = 'Sair';
-}
-
